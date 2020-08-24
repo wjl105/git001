07bf4a4a5965b72ed81eb019c011d574240e1486
wjl105 <1193042775@qq.com>
2020-08-24 15:23:52 +0800

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("first test");
+        System.out.println("second");
+        System.out.println("third");
     }
 }
